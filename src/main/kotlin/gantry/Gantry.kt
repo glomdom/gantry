0e7896b0547a3ea6575c67ab0a21734388a537b1
glomdom <103685817+glomdom@users.noreply.github.com>
2026-03-07 16:35:28 +0200

    feat: register a rough press form

--- src/main/kotlin/gantry/Gantry.kt
+++ src/main/kotlin/gantry/Gantry.kt
@@ -14,6 +14,7 @@ class Gantry : JavaPlugin(), RebarAddon {
         instance = this
 
         registerWithRebar()
+        GantryItems.registerAll()
     }
 
     companion object {
---
+++ src/main/kotlin/gantry/utils/GantryUtils.kt
@@ -0,0 +1,10 @@
+﻿package me.glomdom.gantry.utils
+
+import me.glomdom.gantry.Gantry
+import org.bukkit.NamespacedKey
+
+object GantryUtils {
+    fun gantryKey(key: String): NamespacedKey {
+        return NamespacedKey(Gantry.instance, key)
+    }
+}
