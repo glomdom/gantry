1695be223a4ef67286597634d3b48b2b89bb7ba6
glomdom <103685817+glomdom@users.noreply.github.com>
2026-03-07 16:15:32 +0200

    chore: reorg

---
+++ src/main/kotlin/gantry/Gantry.kt
@@ -0,0 +1,23 @@
+package me.glomdom.gantry
+
+import io.github.pylonmc.rebar.addon.RebarAddon
+import org.bukkit.Material
+import org.bukkit.plugin.java.JavaPlugin
+import java.util.Locale
+
+class Gantry : JavaPlugin(), RebarAddon {
+    override val javaPlugin: JavaPlugin = this
+    override val languages: Set<Locale> = setOf(Locale.ENGLISH)
+    override val material: Material = Material.BLAST_FURNACE
+
+    override fun onEnable() {
+        instance = this
+
+        registerWithRebar()
+    }
+
+    companion object {
+        lateinit var instance: Gantry
+            private set
+    }
+}
