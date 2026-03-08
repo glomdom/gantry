a244a3962d021a6eac3cf42134ff777d1411ca59
glomdom <103685817+glomdom@users.noreply.github.com>
2026-03-09 01:15:01 +0200

    feat: add form input ui item

---
+++ src/main/kotlin/me/glomdom/gantry/utils/GantryGuiItems.kt
@@ -0,0 +1,15 @@
+﻿package me.glomdom.gantry.utils
+
+import io.github.pylonmc.rebar.item.builder.ItemStackBuilder
+import me.glomdom.gantry.utils.GantryUtils.gantryKey
+import net.kyori.adventure.text.Component
+import org.bukkit.Material
+import xyz.xenondevs.invui.item.Item
+
+object GantryGuiItems {
+    fun formInput(): Item =
+        Item.simple(
+            ItemStackBuilder.gui(Material.LIGHT_BLUE_STAINED_GLASS_PANE, gantryKey("form_input"))
+                .name(Component.translatable("gantry.gui.form_input"))
+        )
+}
